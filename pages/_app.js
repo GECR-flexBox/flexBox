fab7cb5d0f732d50a6ddfe8bfc6b2f4837b39a08
KHUNTPRIYANSH <khuntpriyansh1@gmail.com>
2023-04-03 12:37:19 +0530

    fixing aside header

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import Layout from '../components/layout'
-import '@/styles/globals.css'
-
+// import '@/styles/globals.css'
+import '../styles/globals.css'
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
